ad4985fe056a44eeff629b85209b9fa7c788144f
Tehillah <bfgeereigns@gmail.com>
2026-05-12 09:27:02 +0100

    Initial commit: Self Introduction Project

---
+++ intro.js
@@ -0,0 +1,5 @@
+let Name = "Tehillah Jamgbadi";
+let age = 21;
+let favoriteProgrammingLanguage = "Javascript and Rust";
+
+console.log(`My name is ${Name} and i'm ${age} years old, My favorite Programming Languages are ${favoriteProgrammingLanguage}`);
